9b92fa9bb203480ee58c38411b8685e8badfb2eb
Rolexvip <Yamraj0471@outlook.com>
2024-09-17 18:21:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@
 BOT_TOKEN=("7272904241:AAHcNRC3oMt4LWwAYzC1kEAMHh3Az0el9V4")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("@YamrajjXOwner")
+OWNER_USERNAME=("@IPXKING_OWNER")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
 ADMIN_IDS=("6942392489")
